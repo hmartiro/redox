6021b2670d919f2d69c80bc0121af7b57fef2545
Hayk Martirosyan <hayk@skydio.com>
2016-06-13 23:21:16 -0700

    attempt to fix gtest failure

--- make-ci.sh
+++ make-ci.sh
@@ -7,20 +7,22 @@ env | sort
 sudo apt-get update
 sudo apt-get install -y libhiredis-dev libev-dev libgtest-dev redis-server
 
+# Clean
+rm -rf build
+mkdir build
+cd build
+
 # Make gtest
 git clone https://github.com/google/googletest
 cd googletest
-mkdir -p build
+mkdir build
 cd build
-cmake ..
-make
-sudo mv googlemock/gtest/libg* /usr/local/lib/
+cmake -DBUILD_GMOCK=OFF -DBUILD_GTEST=ON -DBUILD_SHARED_LIBS=ON ..
+time make
+sudo mv googletest/libg* /usr/local/lib/
 cd ../..
-rm -rf googletest
 
 # Make redox
-mkdir -p build
-cd build
 cmake -Dexamples=ON -Dlib=ON -Dstatic_lib=ON -Dtests=ON ..
 time make
 ./test_redox
