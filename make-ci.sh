6c15f81141d9ab510735820b857ac90597f29543
Hayk Martirosyan <hayk@skydio.com>
2016-06-13 22:41:19 -0700

    build examples and tests for travis

--- make-ci.sh
+++ make-ci.sh
@@ -3,6 +3,7 @@ set -e
 sudo apt-get install -y libhiredis-dev libev-dev
 mkdir -p build
 cd build
-cmake ..
+cmake -Dexamples=ON -Dlib=ON -Dstatic_lib=ON -Dtests=ON ..
 time make
+./test_redox
 cd ..
