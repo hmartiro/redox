a2a79865aa23d4527aa3852f3278eab7db7233e2
Hayk Martirosyan <hayk@skydio.com>
2016-06-13 23:08:57 -0700

    make-ci script that runs tests

--- make-ci.sh
+++ make-ci.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
-set -e
-sudo apt-get install -y libhiredis-dev libev-dev
+# Build script for continuous integration
+set -ev
+env | sort
+
+# Install packages
+sudo apt-get update
+sudo apt-get install -y libhiredis-dev libev-dev libgtest-dev redis-server
+
+# Make gtest
+git clone https://github.com/google/googletest
+cd googletest
+mkdir -p build
+cd build
+cmake ..
+make
+sudo mv googlemock/gtest/libg* /usr/local/lib/
+cd ../..
+rm -rf googletest
+
+# Make redox
 mkdir -p build
 cd build
 cmake -Dexamples=ON -Dlib=ON -Dstatic_lib=ON -Dtests=ON ..
